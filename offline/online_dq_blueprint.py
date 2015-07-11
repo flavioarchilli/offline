3b0d408fb5764f2a1bddd1d46f85193c69ff95f5
flavioarchilli <flavio.archilli@google.com>
2015-07-11 09:24:04 +0200

    online dataquality monitor has is own blueprint. Histogram pages bugged

---
+++ offline/online_dq_blueprint.py
@@ -0,0 +1,127 @@
+from flask import (
+    Blueprint,
+    current_app,
+    request,
+    flash,
+    redirect,
+    url_for,
+    render_template,
+    abort,
+    jsonify,
+    g
+)
+
+import os
+from glob import glob
+
+from userSettings import *
+from errorhandler import *
+ 
+err = errorhandler()
+settings = userSettings(err)
+settings.readInHistoRootFileIfPossible()
+settings.readInReferenceRootFileIfPossible()
+
+online_dq_bp = Blueprint('online_dq_bp', __name__,
+                     template_folder='templates/online_dq_bp',
+                     static_folder='static')
+
+def check_auth():
+    return current_app.auth
+
+@online_dq_bp.route('/')
+def online_dq():
+
+    if current_app.auth:
+         g.active_page = "online_dq_bp"
+         settings.setOptionsFile(current_app.uid)
+         err.setlogger(current_app.logger)
+         page = render_template("online_dq.html",
+                           LOAD_FROM_DB_FLAG = "false",
+                           REFERENCE_STATE = settings.getReferenceState(),
+                           USERNAME = current_app.username) 
+    else:
+         page = render_template("WelcomePage.html")
+    return page
+
+@online_dq_bp.route('/get_online_dq_filename')
+def get_online_dq_filename():
+
+    mypath = "/hist/Savesets/2015/DQ/DataQuality/"
+    results = [y for x in os.walk(mypath) for y in glob(os.path.join(x[0], '*-EOR.root'))] 
+    filenames = []
+    for j in results:
+        filenames.append(os.path.basename(j))
+    d = dict(
+        status_code = "OK",
+        success = True,
+        data = dict(
+            root_filename = filenames,
+            full_path = results
+            )
+        )
+    return jsonify(d)
+
+@online_dq_bp.route('/set_online_dq_filename')
+def set_online_dq_filename():
+
+    if check_auth() == "false":
+        page = render_template("WelcomePage.html")
+        return page
+
+    filename = request.args.get('filename')
+
+    if (os.path.isfile(filename)):
+        status_filename = settings.setHistoROOTFileName(filename)        
+    
+    if status_filename == False:
+        d = dict(
+            success = False,
+            status_code = 'ROOT_FILE_NOT_FOUND',
+            data = dict(
+                message = "ROOT File not found."
+                )
+            )
+        return jsonify(d)
+    else:
+        d = dict(
+            success = False,
+            status_code = 'ROOT_FILE_FOUND',
+            data = dict(
+                message = "Reference File found."
+                )
+            )
+        return jsonify(d)
+
+@online_dq_bp.route('/set_online_dq_reference_filename')
+def set_online_dq_reference_filename():
+
+    if check_auth() == "false":
+        page = render_template("WelcomePage.html")
+        return page
+
+    filename = request.args.get('filename')
+
+    if (os.path.isfile(filename)):
+        status_filename = settings.setReferenceROOTFileName(filename)        
+    
+    if status_filename == False:
+        d = dict(
+            success = False,
+            status_code = 'REFERENCE_FILE_NOT_FOUND',
+            data = dict(
+                message = "REFERENCE File not found."
+                )
+            )
+        return jsonify(d)
+    else:
+        d = dict(
+            success = False,
+            status_code = 'REFERENCE_FILE_FOUND',
+            data = dict(
+                message = "Reference File found."
+                )
+            )
+        return jsonify(d)
+
+            
---
+++ offline/static/javascripts/online_dq_topbar.js
@@ -0,0 +1,124 @@
+// ONLINE_DQ top bar
+
+// Reads out global variables set by python program and sets toolbar icons accordingly
+function initStatusIndicator() {
+
+  if(filename == "") {
+    setStatusField("Filename not provided!", "warning");
+  } else if(reference_filename == "") {
+    setStatusField("Reference Filename not provided!", "warning");
+  }
+
+}
+
+
+function setStatusField(message, status)
+{
+    $("#statusIndicatorContainer").removeClass("btn-success");
+    $("#statusIndicatorContainer").removeClass("btn-danger");
+    $("#statusIndicatorContainer").removeClass("btn-info");
+    $("#statusIndicatorContainer").removeClass("btn-warning");
+    
+    $("#statusIndicatorIcon").removeClass("glyphicon-ok");
+    $("#statusIndicatorIcon").removeClass("glyphicon-exclamation-sign");
+    $("#statusIndicatorIcon").removeClass("glyphicon-question-sign");
+    
+    if(status == "warning"){
+	$("#statusIndicatorContainer").addClass("btn-warning");
+	$("#statusIndicatorIcon").addClass("glyphicon-question-sign");
+    }else if(status == "danger"){
+	$("#statusIndicatorContainer").addClass("btn-danger");
+	$("#statusIndicatorIcon").addClass("glyphicon-exclamation-sign");
+    }else if(status == "info"){
+	$("#statusIndicatorContainer").addClass("btn-info");
+	$("#statusIndicatorIcon").addClass("glyphicon-exclamation-sign");
+    }else if(status == "success"){
+	$("#statusIndicatorContainer").addClass("btn-success");
+	$("#statusIndicatorIcon").addClass("glyphicon-ok");
+    }else{
+	alert("Unrecognised Status for status field!");
+    }
+		
+    $("#statusIndicatorText").text(message);
+
+}
+
+function setfilename(event){
+    var full_path = event.data.full_path;
+    var visible_name = event.data.visible_name;
+    var reference_flag = event.data.reference_flag;
+    var url = "set_online_dq_filename?filename=";
+    if (reference_flag) url = "set_online_dq_reference_filename?filename=";
+
+    $.ajax({
+	    async : true,
+	    type : "GET",
+	    url : url+encodeURIComponent(fullPath),
+
+	    success : function(json) {
+		set_online_dq_filename_feedback(json.status_code, full_path, visible_name);
+	    },  
+
+	    error : function(xhr, ajaxOptions, thrownError) {
+		alert("JSON Error:" + thrownError);
+		setStatusField("JSON Error:" + thrownError, "danger");
+	    }//,
+		//		complete : function(){disableNavBar(false);}
+	});
+
+}
+
+function build_menu(el, status_code, data, reference_flag){
+    if (status_code == "NO_ACCESS") {
+	console.log("NO access to /hist area");
+    } else if (status_code == "OK") {
+	$(el).empty();
+	for(var i = 0; i < data.root_filename.length; i++) {
+	    var full_path = data.full_path[i];
+	    var visible_name = data.root_filename[i];
+	    var html = '<li role="presentation"><a role="menuitem" data-fullpath="' + 
+		full_path + 
+		'" tabindex="-1" href="#" id="' + 
+		visible_name + 
+		'">' + 
+		visible_name + 
+		'</a></li>';
+	    $(el).append(html);
+      
+	    $("#"+visible_name).on("click", {
+		    "fullpath": full_path,
+		    "visible_name": visible_name, 
+		    "reference_flag": reference_flag}, 
+		function( event ) {
+		    setFilename(event);
+		});
+
+	}
+    }
+}
+
+function init_selector_for_online_dq(el, reference_flag){
+    $.ajax({
+	   async : true, 
+	   type : "GET",
+	   url : "get_online_dq_filename",
+	   success : function(json){
+		build_menu(el, json.status_code, json.data, reference_flag);
+	   },
+	   error : function(xhr, ajaxOptions, thrownError) {
+	       alert("<reconumber> JSON Error:" + thrownError);
+	       //	       setStatusField("JSON Error:" + thrownError, "danger");
+	   }
+	});
+}
+////////////////////////////////////////////
+//jQuery part
+////////////////////////////////////////////
+
+$(function() {
+
+	initStatusIndicator();
+	init_selector_for_online_dq($("#filename_online_dq_DropdownMenu", false));
+	init_selector_for_online_dq($("#reference_filename_online_dq_DropdownMenu", true));
+
+});
