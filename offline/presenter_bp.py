38d758d262f925647ee0d527bc63ac97f892236d
derkach <derkach@gw20.lbdaq.cern.ch>
2015-07-15 11:45:15 +0200

    presenter blueprint moved to a separate file

---
+++ offline/presenter_bp.py
@@ -0,0 +1,24 @@
+from flask import (
+    Blueprint,
+    current_app,
+    request,
+    flash,
+    redirect,
+    url_for,
+    render_template,
+    abort,
+    jsonify,
+    g
+)
+
+presenter = Blueprint('offline', __name__,
+                        template_folder='templates',
+                        static_folder='static',
+                        static_url_path='/{0}'.format(__name__))
+@presenter.route('/')
+def welcome_fun():
+         page = render_template("Hello.html")
+         return page
+   
+
+
